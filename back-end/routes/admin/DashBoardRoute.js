90f216cea560d30ba810d9ad70b546ae3169f481
vanminh <minhp4541@gmail.com>
2023-05-03 17:45:33 +0700

    update stats

--- back-end/routes/admin/DashBoardRoute.js
+++ back-end/routes/admin/DashBoardRoute.js
@@ -1,10 +1,14 @@
 const express = require("express");
 
 const { verifyAdmin } = require("../../middleware/VerifyAdmin");
-const { revenueSale } = require("../../controllers/DashBoardController");
+const {
+  revenueSale,
+  storeStats,
+} = require("../../controllers/DashBoardController");
 
 const router = express.Router();
 
 router.post("/revenuesale", verifyAdmin, revenueSale);
+router.post("/stats", verifyAdmin, storeStats);
 
 module.exports = router;
